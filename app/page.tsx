eafa74ab5d8aacc88d7f58198a343cb4f0d563b3
loggs <lcsugit@gmail.com>
2025-05-08 16:01:05 -0400

    first page animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,46 @@
-'use client';
-import Image from "next/image";
+import * as motion from "motion/react-client"
+import "./page.css";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Hello, Next.js!</h1>
-      <Image
-        src="/next.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-        priority
-      />
+    <main className="flex min-h-screen flex-col items-center justify-between">        
+        <motion.div
+            className="triangle"
+            initial={{ offsetDistance: "0%" }}
+            animate={{ offsetDistance: "100%" }}
+            transition={{ delay: 0.2, duration: 6, ease: "easeInOut" }}
+        />
+        <motion.div
+            className="triangle2"
+            initial={{ offsetDistance: "0%" }}
+            animate={{ offsetDistance: "100%" }}
+            transition={{ delay: 0.1, duration: 6, ease: "easeInOut" }}
+        />
+        <motion.div
+            className="triangle3"
+            initial={{ offsetDistance: "0%" }}
+            animate={{ offsetDistance: "100%" }}
+            transition={{ delay: 0.3, duration: 6, ease: "easeInOut" }}
+        />
+        <motion.div
+            className="triangle4"
+            initial={{ offsetDistance: "0%" }}
+            animate={{ offsetDistance: "100%" }}
+            transition={{ delay: 1, duration: 6, ease: "easeInOut" }}
+        />
+        <motion.div
+            className="triangle5"
+            initial={{ offsetDistance: "0%" }}
+            animate={{ offsetDistance: "100%" }}
+            transition={{ delay: 1, duration: 6, ease: "easeInOut" }}
+        />
+        <motion.div
+            className="triangle6"
+            initial={{ offsetDistance: "0%" }}
+            animate={{ offsetDistance: "100%" }}
+            transition={{ delay: 1.5, duration: 6, ease: "easeInOut" }}
+        />
     </main>
   );
 }
